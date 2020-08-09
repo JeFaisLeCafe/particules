5d523f21e40cc717162df13ce366b66e31da908b
Pierre-Etienne Soury <33933332+JeFaisLeCafe@users.noreply.github.com>
2020-08-09 12:37:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ window.addEventListener("mousemove", (e) => {
 
 ctx.fillStyle = "red";
 ctx.font = "27px Verdana";
-ctx.fillText("SOURY", 0, 0);
+ctx.fillText("SOURY", 0, 10);
 const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 // Particule
